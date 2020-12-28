fb013ea4ed4f058f02439be2e3d079e07521906a
liuchu <1371772034@qq.com>
2020-12-28 17:59:02 +0800

    update

--- client/src/components/Head.vue
+++ client/src/components/Head.vue
@@ -1,48 +1,32 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="映点论坛" name="first">用户管理</el-tab-pane>
+      <el-tab-pane label="团队讨论" name="second">团队讨论</el-tab-pane>
+      <el-tab-pane label="个人空间" name="third">个人空间</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "head"
+   export default {
+    data() {
+      return {
+        activeName: 'second'
+      };
+    },
+    methods: {
+      handleClick(tab, event) {
+         if(this.activeName === "second"){
+              this.$router.push({
+                path:'/posts',
+                name:'Posts',
+                params:{}
+              })
+         }
+      }
     }
+  };
 </script>
 
 <style scoped>
